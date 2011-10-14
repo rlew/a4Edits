060a08b1557cc32cb7ae573f2975f08e17d8fcb2
Trevor <trevorrjohn@gmail.com>
2011-10-14 12:59:22 -0300

    formating and changed

--- applyDecomp.c
+++ applyDecomp.c
@@ -7,9 +7,9 @@ void applyDecompToRGBInt(int col, int row, A2 toBeFilled,
     struct Pnm_rgb* decomped = ptr;
     struct rgbFloat* original = mycl->methods->at(mycl->array,col, row);
     unsigned denominator = mycl->denom;
-    decomped->red = (unsigned)(original->red * denominator);
-    decomped->green = (unsigned)(original->green * denominator);
-    decomped->blue = (unsigned)(original->blue * denominator);
+    decomped->red = (unsigned)(original->red) * denominator;
+    decomped->green = (unsigned)(original->green) * denominator;
+    decomped->blue = (unsigned)(original->blue) * denominator;
 }
 
 void applyDecompToRGBFloat(int col, int row, A2 toBeFilled, 
@@ -18,12 +18,12 @@ void applyDecompToRGBFloat(int col, int row, A2 toBeFilled,
     struct Closure *mycl = cl;
     struct rgbFloat *toBeSet = ptr;
     struct YPP* original = mycl->methods->at(mycl->array, col, row);
-    toBeSet->red = 1.0 * original->y + 0.0 * original->pb + 1.402 *
-        original->pr;
-    toBeSet->green = 1.0 * original->y - 0.344136 * original->pb - 
-        0.714136 * original->pr;
-    toBeSet->blue = 1.0 * original->y + 1.772 * original->pb + 0.0 * 
-        original->pr;
+    toBeSet->red = 1.0 * original->y + 0.0 * original->pb
+                                                + 1.402 * original->pr;
+    toBeSet->green = 1.0 * original->y - 0.344136 * original->pb 
+                                                - 0.714136 * original->pr;
+    toBeSet->blue = 1.0 * original->y + 1.772 * original->pb 
+                                                    + 0.0 * original->pr;
 }
 
 void applyDecompToYPP(int col, int row, A2 toBeFilled, 
